feb908511be1bf0c89029722c78c1df92bd1b564
Edson Rodrigues <rodriguesedson.dev@gmail.com>
2024-09-10 23:53:10 -0300

    feat: add about and contacts sections

--- script.js
+++ script.js
@@ -2,13 +2,6 @@ const header_btn = document.getElementById('header-btn')
 const aside = document.getElementById('aside');
 const aside_logo = document.getElementById('aside-logo');
 const aside_link = document.getElementsByClassName('aside-link');
-//TODO: to implement
-const header_link_about = document.getElementById('header-link-about');
-const header_link_contacts = document.getElementById('header-link-contacts');
-const aside_link_about = document.getElementById('aside_link_about');
-const aside_link_contacts = document.getElementById('aside-link-contacts');
-const links_to_implement = [header_link_contacts, header_link_about, aside_link_contacts, aside_link_about];
-//end
 
 header_btn.addEventListener('click', openMenu);
 aside_logo.addEventListener('click', closeMenu);
@@ -22,13 +15,4 @@ function openMenu() {
 
 function closeMenu() {
     aside.style.display = 'none';
-}
-
-//alert to implement
-Array.from(links_to_implement).forEach(item => {
-    item.addEventListener('click', displayMessage);
-});
-
-function displayMessage() {
-    alert('Seção em manutenção');
 }
